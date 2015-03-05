1b2f9a61a59f75beef2f4f3015fcc8595c76e7b2
ewanosb <ewanbakhuis97@live.nl>
2015-03-05 17:31:26 +0100

    Weed object toegevoegd

---
+++ Source/ThugLife/src/nl/highco/thuglife/objects/Weed.java
@@ -0,0 +1,41 @@
+package nl.highco.thuglife.objects;
+
+import nl.highco.thuglife.Score;
+import nl.saxion.act.playground.model.GameBoard;
+import nl.saxion.act.playground.model.GameObject;
+
+public class Weed extends GameObject {
+	
+	public static final String WEED_IMAGE = "weed";
+	
+
+
+	@Override
+	public String getImageId() {
+		return WEED_IMAGE;
+	}
+
+	@Override
+	public void onTouched(GameBoard gameBoard) {
+		
+		Score score = new Score();
+		
+		int newPosX = (int) (Math.random() * gameBoard.getWidth());
+		int newPosY = (int) (Math.random() * gameBoard.getHeight());
+		
+		gameBoard.addGameObject(this, newPosX, newPosY);
+		int oudeScore = score.getScore();
+		int newScore = oudeScore + 50;
+		score.updateScore(newScore);
+		gameBoard.updateView();
+	}
+
+
+
+
+}
+	
+	
+
+	
+
